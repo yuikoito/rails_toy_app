18ad902c04fb8d840d97d27e7d74a63a7aae5e4f
yuikoito <toffywing@gmail.com>
2021-12-16 18:20:20 +0900

    feat: add microposts and validate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_15_101756) do
+ActiveRecord::Schema.define(version: 2021_12_16_085244) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
